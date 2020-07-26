4ae26036dc14612f8b8aaa8fc0be0bdcd68efcdc
jhansi8899 <68681145+jhansi8899@users.noreply.github.com>
2020-07-26 12:57:29 +0530

    Add files via upload

---
+++ epam/src/main/java/com/epam/AA_Cal/App.java
@@ -0,0 +1,21 @@
+package com.epam.dp_Calci;
+public class App
+{
+	/*ALL Operations*/
+	public int addition(int a,int b)
+	{
+		return a+b;
+	}
+	public  int subtraction(int a,int b)
+	{
+		return a-b;
+	}
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+	public int division(int a,int b)
+	{
+		return a/b;
+	}
+}
